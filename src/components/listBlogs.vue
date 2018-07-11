38ee7f45ff808f23c849de9714a1add4c07310c9
akhilesh <akhileshrao19@gmail.com>
2018-07-11 21:40:13 +0530

    mixins

---
+++ src/components/listBlogs.vue
@@ -0,0 +1,53 @@
+<template>
+  <div id="show-blogs">
+    <h1>List Blog Title</h1>
+    <input type="text" v-model="search" class="search-bar" placeholder="search blogs">
+    <div v-for="blog in filterBlogs" class="single-blog">
+      <h2 v-rainbow> {{blog.title | to-uppercase}}</h2>
+    </div>
+  </div>
+</template>
+
+<script>
+  import searchMixin from '../mixins/searchMixin'
+    export default {
+      name: "listBlogs",
+      data() {
+        return {
+          blogs: [],
+          search: ""
+        }
+      },
+      mixins:[searchMixin],
+      created() {
+        this.$http.get('https://jsonplaceholder.typicode.com/posts')
+          .then((data) => {
+            // console.log(data);
+            this.blogs = data.body.slice(0, 10);
+            console.log(this.blogs);
+          })
+          .catch(err => {
+            console.log(err);
+          })
+      }
+    }
+</script>
+
+<style scoped>
+  #show-blogs{
+    max-width: 802px;
+    margin : 0 auto
+  }
+  .single-blog{
+    padding: 20px;
+    margin: 20px 0 20px 0;
+    background: #eee;
+
+    box-sizing: border-box;
+  }
+  .search-bar{
+    position: relative;
+    left : 100%;
+    transform: translateX(-100%);
+  }
+</style>
--- src/components/showBlogs.vue
+++ src/components/showBlogs.vue
@@ -10,6 +10,7 @@
 </template>
 
 <script>
+  import searchMixin from '../mixins/searchMixin'
     export default {
       name: "showBlogs",
       data(){
@@ -18,16 +19,7 @@
           search:""
         }
       },
-      methods:{
-
-      },
-      computed:{
-        filterBlogs(){
-            return this.blogs.filter((blog)=>{
-            return blog.title.includes(this.search);
-          })
-        }
-      },
+      mixins:[searchMixin],
       created(){
           this.$http.get('https://jsonplaceholder.typicode.com/posts')
             .then((data)=>{
@@ -38,35 +30,6 @@
             .catch(err=>{
                 console.log(err);
             })
-      },
-      filters:{
-        'to-uppercase':(value)=>{
-          return value.toUpperCase();
-        },
-        'snippet': (value)=> {
-          return value.slice(0, 100) + '...';
-        }
-      },
-      directives:{
-        'rainbow': {
-          bind(el, binding, vnode) {
-            el.style.color = "#" + Math.random().toString().slice(2, 8);
-          }
-        },
-        'theme': {
-          bind(el, binding, vnode) {
-            if (binding.value == 'wide') {
-              el.style.maxWidth = '1200px';
-            } else if (binding.value == "narrow") {
-              el.style.maxWidth = '800px';
-            }
-
-            if (binding.arg == 'column') {
-              el.style.background = '#ddd';
-              el.style.padding = '20px';
-            }
-          }
-        }
       }
     }
 </script>
