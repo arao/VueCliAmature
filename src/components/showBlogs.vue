bafedeffd9d688d0173c3691261aa0a063c62421
akhilesh <akhileshrao19@gmail.com>
2018-07-11 21:24:46 +0530

    done local registration of filter and directives

--- src/components/showBlogs.vue
+++ src/components/showBlogs.vue
@@ -38,6 +38,35 @@
             .catch(err=>{
                 console.log(err);
             })
+      },
+      filters:{
+        'to-uppercase':(value)=>{
+          return value.toUpperCase();
+        },
+        'snippet': (value)=> {
+          return value.slice(0, 100) + '...';
+        }
+      },
+      directives:{
+        'rainbow': {
+          bind(el, binding, vnode) {
+            el.style.color = "#" + Math.random().toString().slice(2, 8);
+          }
+        },
+        'theme': {
+          bind(el, binding, vnode) {
+            if (binding.value == 'wide') {
+              el.style.maxWidth = '1200px';
+            } else if (binding.value == "narrow") {
+              el.style.maxWidth = '800px';
+            }
+
+            if (binding.arg == 'column') {
+              el.style.background = '#ddd';
+              el.style.padding = '20px';
+            }
+          }
+        }
       }
     }
 </script>
